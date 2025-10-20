28634cd59632b06e5103154c781a6326dc2dcfbc
Flávio Costa e Silva <flaviocosta@outlook.com>
2025-10-20 00:26:50 -0300

    Fix api redirect when not authenticated

--- app/app/Http/Middleware/Authenticate.php
+++ app/app/Http/Middleware/Authenticate.php
@@ -2,20 +2,42 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return string|null
      */
-    protected function redirectTo($request)
+    protected function redirectTo($request): ?string
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if ($request->expectsJson() || $request->is('api/*')) {
+            return null;
         }
+
+        return route('login');
+    }
+
+    /**
+     * Handle an unauthenticated user.
+     *
+     * @param Request $request
+     * @param  array  $guards
+     * @return void
+     *
+     * @throws AuthenticationException
+     */
+    protected function unauthenticated($request, array $guards): void
+    {
+        throw new AuthenticationException(
+            'Not authenticated',
+            $guards,
+            $this->redirectTo($request)
+        );
     }
 }
